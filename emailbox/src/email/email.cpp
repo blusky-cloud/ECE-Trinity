9fa9c606110320a37e7c8b6fca7777e6ad051725
Benjamin-C <bencrall@gmail.com>
2021-11-11 17:22:52 -0800

    Added some comments

--- emailbox/src/email/email.cpp
+++ emailbox/src/email/email.cpp
@@ -1,3 +1,9 @@
+/* Source code for the EmailClient object - email.cpp
+ *
+ * Creates an IMAP connection to a mail server (as specified in credentials.h)
+ * and helps with a few basic uses, like getting the latest unread email.
+ */
+
 #include "./email.h"
 
 #include <Arduino.h>
@@ -6,82 +12,66 @@
 #include "../../credentials.h"
 #include "../../src/debug/debugger.h"
 #include "../../imap.google.com.cert.h"
+
 /*
-
-When first connect:
-  Login:
-  <tag> LOGIN <email> <password>
-
-  Select the inbox:
-  <tag> SELECT INBOX
-
-On Reload:
-  Noop to update stuff:
-  <tag> NOOP
-
-  Find unread emails
-  <tag> SEARCH UNSEEN
-
-  Get email header
-  <tab> FETCH <number> BODY.PEEK[HEADER.FIELDS (SUBJECT)]
+ * IMAP commands used here
+ * 
+ * When first connect:
+ *   Login:
+ *   <tag> LOGIN <email> <password>
+ * 
+ *   Select the inbox:
+ *   <tag> SELECT INBOX
+ * 
+ * On Reload:
+ *   Noop to update stuff:
+ *   <tag> NOOP
+ * 
+ *   Find unread emails
+ *   <tag> SEARCH UNSEEN
+ * 
+ *   Get email header
+ *   <tab> FETCH <number> BODY.PEEK[HEADER.FIELDS (SUBJECT)]
 */
 
-#define END_IMAP_COMMAND "\r\n"
-
+// Uncomment this line to print more verbose debug info
 // #define VERBOSE_DEBUG
 
-EmailClient::EmailClient(Debugger* dbgr) {
-  dbg = dbgr;
-}
-
+// Go look for the email object, it should be created in emailbox.ino
 extern EmailClient email;
 
+/*
+ * Callback for the email command.
+ *
+ * Has options connect, refresh, logout, check, printIMAP, silentIMAP, help
+ */
 void emailCommand(int argc, char* argv[], Debugger* dbg) {
   if(argc < 2) {
     // email.updateEmails();
   } else {
-    if(!strcmp(argv[1], "status")) {
-      Serial.println("Getting status ...");
-      // bool sts = imap.sendCustomCommand("A042 STATUS INBOX (UNSEEN)", &statusCommandCallback);
-      // Serial.printf("%s %s", resp, (sts) ? "yes" : "no");
-    // } else if(!strcmp(argv[1], "command")) {
-      // if(argc < 4) {
-      //   Serial.println("Not enough arguments.");
-      // } else {
-      //   char cmd[128];
-      //   char* p = cmd;
-      //   for(int i = 2; i < argc; i++) {
-      //     char* c = argv[i];
-      //     if(i != 0) {
-      //       *p = 0x20;
-      //       p++;           
-      //     }
-      //     while(*c >= 0x20) {
-      //       *p = *c;
-      //       p++; c++;
-      //     }
-      //   }
-      //   *p = '\r'; p++;
-      //   *p = '\n'; p++;
-      //   *p = '\0';
-      //   Serial.printf("C: > %s\n", cmd);
-
-      //   const int max_result_len = 1024;
-      //   char result[max_result_len];
-      //   email.issueCommand(argv[2], cmd, result, max_result_len);
-
-      //   Serial.println("Sending command ...");
-      // }
-    } else if(!strcmp(argv[1], "readline")) {
-      Serial.println("Next line ---");
-      email.readPrintLine();
-      Serial.println("End line  ---");
     } else if(!strcmp(argv[1], "connect")) {
       Serial.println("Connecting ...");
       email.connect();
     } else if(!strcmp(argv[1], "refresh")) {
       Serial.println("Refreshing ...");
       email.refresh();
+    } else if(!strcmp(argv[1], "logout")) {
+      Serial.println("Logging out");
+      email.logout();
+    } else if(!strcmp(argv[1], "check")) {
+      if(email.hasUnseen()) {
+        char subj[MAX_EMAIL_SUBJECT_LENGTH];
+        email.getLatestSubject(subj);
+        Serial.printf("Latest Unread: %s\n", subj);
+      } else {
+        Serial.println("No unread messages.");
+      }
+    } else if(!strcmp(argv[1], "printIMAP")) {
+      email.setPrintIMAPresponses(true);
+    } else if(!strcmp(argv[1], "silentIMAP")) {
+      email.setPrintIMAPresponses(false);
+    } else if(!strcmp(argv[1], "help")) {
+      Serial.println("email commands: connect, refresh, logout, check, printIMAP, silentIMAP, help");
     } else {
       Serial.print("No can do da thingy "); 
       Serial.println(argv[1]); 
@@ -89,6 +79,21 @@ void emailCommand(int argc, char* argv[], Debugger* dbg) {
   }
 }
 
+/*
+ *  Constructor for the Email Client
+ */
+EmailClient::EmailClient(Debugger* dbgr) {
+  dbg = dbgr;
+}
+
+/*
+ * Begins the EmailClient. Must be called first.
+ * Seperated from constructor since some Arduino functions don't work before setup()
+ *
+ * Connects to the WiFi and sets up (but does not connect to)
+ *    the TLS connection to the IMAP server. The WiFi credentials
+ *    are stored in credentials.h
+ */
 void EmailClient::begin(void) {
   
   dbg->registerCommand("email", &emailCommand);
@@ -110,11 +115,20 @@ void EmailClient::begin(void) {
   Serial.println();
 }
 
-bool EmailClient::isConnected() {
+/*
+ * Checks if the connection is active. You may call connect() again
+ *   if the connection isn't active to re-open it.
+ */
+bool EmailClient::isConnected(void) {
   return client.connected();  
 }
 
-bool EmailClient::connect() {
+/*
+ * Connects to the IMAP server through TLS. The IMAP server credentials
+ *    are stored in credentials.h. This can be called multiple times,
+ *    and can be used to restore a dead connection.
+ */
+bool EmailClient::connect(void) {
   /* set SSL/TLS certificate */
   client.setCACert(ca_cert);
 
@@ -132,6 +146,89 @@ bool EmailClient::connect() {
   }
 }
 
+/*
+ * Logs out of the IMAP server. You can log in again later with connect()
+ */
+void EmailClient::logout(void) {
+  executeCommand("lgo", "LOGOUT");
+  client.stop();
+}
+
+/*
+ * Gets the latest emails from the server. Also updates the
+ *    hasUnread and lastUnread fields.
+ */
+void EmailClient::refresh(void) {
+  Serial.println("Reading messages ...");
+  executeCommand("rfh", "NOOP");
+  executeCommand("src", "SEARCH UNSEEN");
+  char* p = response;
+
+  while(*p++ != '\n' && *p != '\r') {}
+  p--;
+  int dts = 0;
+  int id = 0;
+  int mlt = 1;
+  
+  while(*p >= '0' && *p <= '9') {
+    dts++;
+    id += (*p - '0') * mlt;
+    mlt *= 10;
+    p--;
+  }
+
+  if(dts > 0) {
+    hasUnread = true;
+    // <tab> FETCH <number> 
+    char cmd[MAX_IMAP_COMMAND_LENGTH];
+    sprintf(cmd, "FETCH %d BODY.PEEK[HEADER.FIELDS (SUBJECT)]", id);
+    executeCommand("fth", cmd);
+
+    p = response;
+    
+    char tempsubj[MAX_EMAIL_SUBJECT_LENGTH];
+    char* q = tempsubj;
+    while(*p++ != '\n' && *p != '\r') {}
+    while(*p++ == '\n' || *p == '\r') {}
+    p += 9;
+    while(*p != '\n' && *p != '\r') {
+      *q = *p;
+      q++; p++;
+    }
+    *q = '\0';
+    sprintf(lastUnread, "%s", tempsubj);
+  } else {
+    hasUnread = false;
+    lastUnread[0] = '\0';
+  } 
+}
+
+/*
+ * Gets wether there are unseen messages in the inbox
+ */
+bool EmailClient::hasUnseen(void) {
+  return hasUnread;
+}
+
+/*
+ * Gets the subject line of the latest unseen message.
+ * Copies it to whereToPut
+ */
+void EmailClient::getLatestSubject(char* whereToPut) {
+  strcpy(whereToPut, lastUnread);
+}
+
+/*
+ * Issues a command. Returns as soon as the command has been issued.
+ *
+ * You must call readLine or readToEnd to get the response. If you don't,
+ *    the next command executed may get the response from this one.
+ * Also re-connects to the server if needed.
+ * Prints to seril if printIMAPresponses is true
+ *
+ * tag must be a pointer c style string with 3 characters + a null.
+ * command is a pointer to a c style string holding the rest of the command 
+ */ 
 void EmailClient::issueCommand(char* tag, char* command) {
   if(!isConnected()) {
     connect();
@@ -146,11 +243,26 @@ void EmailClient::issueCommand(char* tag, char* command) {
   }
 }
 
+/*
+ * Issues a command. Waits to return until the response has been recorded
+ *
+ * Returns true if the IMAP command returned "OK", false if it returne "BAD"
+ * Stores the response to this object's response field.
+ *    
+ * Also re-connects to the server if needed.
+ *
+ * tag must be a pointer c style string with 3 characters + a null.
+ * command is a pointer to a c style string holding the rest of the command 
+ */ 
 bool EmailClient::executeCommand(char* tag, char* command) {
   issueCommand(tag, command);
   return readToEnd(tag);
 }
 
+/*
+ * Reads the next line of the IMAP response and stores it to this object's response field.
+ * Prints to seril if printIMAPresponses is true
+ */
 void EmailClient::readLine(void) {
   char c = 'q';
   int savedChars = 0;
@@ -174,6 +286,9 @@ void EmailClient::readLine(void) {
   }
 }
 
+/*
+ * Reads the next line of the IMAP response and prints it to serial.
+ */
 void EmailClient::readPrintLine(void) {
   char c = 'q';
   while(c != '\n') {
@@ -184,33 +299,12 @@ void EmailClient::readPrintLine(void) {
   }
 }
 
-bool found(const char* last, const char* search, int lastlength, int searchlength) {
-#ifdef VERBOSE_DEBUG
-  Serial.printf("[%s:%d\t\t%s:%d]", last, strlen(last), search, searchlength);
-#endif
-
-  if(searchlength <= lastlength) {
-    int laststart = lastlength - searchlength;
-    int count = 0;
-
-    for(int i = 0; i < searchlength; i++) {
-      if(last[laststart + i] != search[i]) {
-        return false;
-      }
-    }
-
-    return true;
-  
-  } else {
-    return false;
-  }
-}
-
-void EmailClient::setPrintIMAPresponses(bool toPrint) {
-  printIMAPresponses = toPrint;
-}
-
-// Tag MUST be 3 characters
+/*
+ * Reads the entire remaining IMAP response until tag and "OK" or "BAD" are found,
+ *    then finishes that line. Stores result to this object's response field.
+ *
+ * tag must be a pointer c style string with 3 characters + a null.
+ */ 
 bool EmailClient::readToEnd(char* tag) {
   char findOK[7];
   strcpy(findOK, tag);
@@ -291,55 +385,34 @@ bool EmailClient::readToEnd(char* tag) {
   return status;
 }
 
-void EmailClient::refresh(void) {
-  Serial.println("Reading messages ...");
-  executeCommand("rfh", "NOOP");
-  executeCommand("src", "SEARCH UNSEEN");
-  char* p = response;
+/*
+ * Sets wether or not to print the IMAP debugging info to serial
+ */
+void EmailClient::setPrintIMAPresponses(bool toPrint) {
+  printIMAPresponses = toPrint;
+}
 
-  while(*p++ != '\n' && *p != '\r') {}
-  p--;
-  int dts = 0;
-  int id = 0;
-  int mlt = 1;
-  
-  while(*p >= '0' && *p <= '9') {
-    dts++;
-    id += (*p - '0') * mlt;
-    mlt *= 10;
-    p--;
-  }
+/*
+ * Private method used to determine if the end of the IMAP response has been found.
+ */ 
+bool EmailClient::found(const char* last, const char* search, int lastlength, int searchlength) {
+#ifdef VERBOSE_DEBUG
+  Serial.printf("[%s:%d\t\t%s:%d]", last, strlen(last), search, searchlength);
+#endif
 
-  if(dts > 0) {
-    hasUnread = true;
-    // <tab> FETCH <number> 
-    char cmd[MAX_IMAP_COMMAND_LENGTH];
-    sprintf(cmd, "FETCH %d BODY.PEEK[HEADER.FIELDS (SUBJECT)]", id);
-    executeCommand("fth", cmd);
+  if(searchlength <= lastlength) {
+    int laststart = lastlength - searchlength;
+    int count = 0;
 
-    p = response;
-    
-    char tempsubj[MAX_EMAIL_SUBJECT_LENGTH];
-    char* q = tempsubj;
-    while(*p++ != '\n' && *p != '\r') {}
-    while(*p++ == '\n' || *p == '\r') {}
-    p += 9;
-    while(*p != '\n' && *p != '\r') {
-      *q = *p;
-      q++; p++;
+    for(int i = 0; i < searchlength; i++) {
+      if(last[laststart + i] != search[i]) {
+        return false;
+      }
     }
-    *q = '\0';
-    sprintf(lastUnread, "%s", tempsubj);
+
+    return true;
+  
   } else {
-    hasUnread = false;
-    lastUnread[0] = '\0';
-  } 
-}
-
-bool EmailClient::hasUnseen(void) {
-  return hasUnread;
-}
-
-void EmailClient::getLatestSubject(char* whereToPut) {
-  strcpy(whereToPut, lastUnread);
+    return false;
+  }
 }
--- emailbox/src/email/email.h
+++ emailbox/src/email/email.h
@@ -1,3 +1,9 @@
+/* Header for the EmailClient object - email.h
+ *
+ * Creates an IMAP connection to a mail server (as specified in credentials.h)
+ * and helps with a few basic uses, like getting the latest unread email.
+ */
+
 #ifndef EMAIL_H
 #define EMAIL_H
 
@@ -6,36 +12,45 @@
 
 #include "../../src/debug/debugger.h"
 
+// The below paramaters can be incresed, although this will require more memory.
+// The maximum length of an email subject line and other included data
 #define MAX_EMAIL_SUBJECT_LENGTH 256
+// The maximum length of an IMAP command
 #define MAX_IMAP_COMMAND_LENGTH 64
+// The maximum length of an IMAP response
 #define MAX_IMAP_RESPONSE_LENGTH 1024
+// The line ending for the IMAP commands
+#define END_IMAP_COMMAND "\r\n"
 
+// Debug email command callback
 void emailCommand(int argc, char* argv[], Debugger* dbg);
 
+// The main Email Client object for email uses
 class EmailClient {
   public:
     EmailClient(Debugger* dbg);
-    void begin(void);
-    void refresh(void);
-    bool hasUnseen(void);
-    void getLatestSubject(char* whereToPut);
-    bool connect();
-    bool isConnected();
-    void issueCommand(char* tag, char* command);
-    bool executeCommand(char* tag, char* command);
-    void readLine(void);
-    void readPrintLine(void);
-    bool readToEnd(char* tag);
-    void setPrintIMAPresponses(bool toPrint);
+    void begin(void);                                 // Sets up the object. Should only be needed once
+    bool isConnected(void);                               // Checks if the connection is still active
+    bool connect(void);                                   // Connects to the IMAP server. Should be called anytime the server disconnects.
+    void logout(void)                                 // Logs out of the IMAP server. You can later log in again with connect().
+    void refresh(void);                               // Updates the latest unread subject variables
+    bool hasUnseen(void);                             // Gets wether there are unseen messages in the inbox
+    void getLatestSubject(char* whereToPut);          // Gets the subject line of the latest unseen message
+    void issueCommand(char* tag, char* command);      // Issues an IMAP command. Does not wait for response
+    bool executeCommand(char* tag, char* command);    // Issues an IMAP command and waits for the response
+    void readLine(void);                              // Reads the next line of the IMAP response to the response variable
+    void readPrintLine(void);                         // Prints the next line of the IMAP response
+    bool readToEnd(char* tag);                        // Reads the rest of the IMAP response
+    void setPrintIMAPresponses(bool toPrint);         // Sets wether to print the IMAP responses to debug
   private:
-    WiFiClientSecure client;
-    Debugger* dbg;
-    bool printIMAPresponses = false;
-    char response[MAX_IMAP_RESPONSE_LENGTH];
-    bool hasUnread;
-    char lastUnread[MAX_EMAIL_SUBJECT_LENGTH];
+    WiFiClientSecure client;                          // The SSL connectoin object
+    Debugger* dbg;                                    // A pointer to the system debugger object
+    bool printIMAPresponses = false;                  // Wether or not to print the IMAP responses
+    char response[MAX_IMAP_RESPONSE_LENGTH];          // The latest processed IMAP response
+    bool hasUnread;                                   // If there are unread messages in the inbox
+    char lastUnread[MAX_EMAIL_SUBJECT_LENGTH];        // The subject line of the latest unread message
+    // Check if a string has been found. Used to find the end of the IMAP responses.
+    bool found(const char* last, const char* search, int lastlength, int searchlength);
 };
-
-bool found(const char* last, const char* search, int lastlength, int searchlength);
 
 #endif
